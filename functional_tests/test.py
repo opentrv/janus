5521ddd5fbefbce246d570f1e3f412ef24fa5132
David Voong <voong.david@gmail.com>
2015-11-12 16:54:47 +0000

    Add test to send a measurement to the udp server and extract it via the dataserver api

--- functional_tests/test.py
+++ functional_tests/test.py
@@ -1,13 +1,14 @@
 import time
 import os
 import subprocess
-from django.test import TestCase
+import requests
+from django.test import TestCase, LiveServerTestCase
 
 tests_dir = '.temp/tests'
 if not os.path.exists(tests_dir):
     os.makedirs(tests_dir)
 
-class FunctionalTest(TestCase):
+class FunctionalTest(LiveServerTestCase):
     log_filename = 'udp_server.log'
 
     def test(self):
@@ -30,6 +31,35 @@ class FunctionalTest(TestCase):
         f = open(self.log_filepath, 'rb')
         line = f.readline()
         self.assertIn(msg, line)
+
+        # send some opentrv data to the udp server
+        msg = '[ "2015-01-01T00:00:43Z", "", {"@":"0a45","+":2,"vac|h":9,"T|C16":201,"L":0} ]'
+        subprocess.check_call(['python', 'manage.py', 'send_udp', msg])
+
+        # use the api to extract the data
+        response = requests.get(os.path.join(self.live_server_url, 'dataserver', 'api', 'opentrv'), params={'date': '2015-01-01'})
+        expected = [
+            {
+                'datetime': "2015-01-01T00:00:43Z",
+                'sensor_id': "0a45",
+                'vacancy': 9
+            },
+            {
+                'datetime': "2015-01-01T00:00:43Z",
+                'sensor_id': "0a45",
+                'temperature': 12.5625
+            },
+            {
+                'datetime': "2015-01-01T00:00:43Z",
+                'sensor_id': "0a45",
+                'light': 0
+            }
+        ]
+
+        try:
+            self.assertEqual(response.json(), expected)
+        except ValueError as e:
+            raise Exception('{}\n{}'.format(e, response.text))
         
         self.fail('TODO')
 
@@ -45,8 +75,8 @@ class FunctionalTest(TestCase):
         self.test_dir = tests_dir + '/{}'.format(len(test_numbers))
         os.makedirs(self.test_dir)
         self.log_filepath = self.test_dir + '/{}'.format(self.log_filename)
-            
-        TestCase.__init__(self, *args, **kwargs)
+    
+        LiveServerTestCase.__init__(self, *args, **kwargs)
         
     def setUp(self):
         # if not os.path.exists(self.tests_dir):
