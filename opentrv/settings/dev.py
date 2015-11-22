78cd20885cd07c6d410ba9efd8b1f9652cc1bb7b
David Voong <voong.david@gmail.com>
2015-11-22 15:40:59 +0000

    Add deployment scripts and virtual machine scripts

--- opentrv/settings/dev.py
+++ opentrv/settings/dev.py
@@ -3,10 +3,10 @@ from base import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mydb',                      # Or path to database file if using sqlite3.
+        'NAME': 'opentrv_db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'dvoong',
-        'PASSWORD': 'password',
+        'USER': 'opentrv',
+        'PASSWORD': 'secret',
         'HOST': 'localhost',                      # Empty for localhost through domain sockets or           '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
