00a4d65c5482f52a187d104f7fd5838ff4a6281d
David Voong <voong.david@gmail.com>
2015-11-23 12:51:53 +0000

    Add query builder for the opentrv_sensor data api method

--- opentrv_sensor/tests/test_views.py
+++ opentrv_sensor/tests/test_views.py
@@ -5,23 +5,15 @@ from django.http import HttpRequest
 from django.test import TestCase
 from opentrv_sensor import views
 
+@mock.patch('opentrv_sensor.views.build_query')
 @mock.patch('opentrv_sensor.views.JsonResponse')
 @mock.patch('opentrv_sensor.views.Measurement')
 class TestAPI(TestCase):
 
     def setUp(self):
         self.request = HttpRequest()
-        self.request.GET['date'] = '2015-01-01'
 
-    def test_filters_measurement_objects_with_request_date(self, Measurement, JsonResponse):
-
-        response = views.api(self.request)
-
-        date = datetime.datetime(2015, 1, 1).date()
-        Measurement.objects.filter.assert_called_once_with(datetime__year=date.year, datetime__month=date.month, datetime__day=date.day)
-        
-
-    def test_converts_measurements_into_dictionaries(self, Measurement, JsonResponse):
+    def test_converts_measurements_into_dictionaries(self, Measurement, JsonResponse, build_query):
 
         measurements = mock.Mock()
         Measurement.objects.filter.return_value = measurements
@@ -30,7 +22,8 @@ class TestAPI(TestCase):
 
         Measurement.to_dict.assert_called_once_with(measurements)
 
-    def test_response_content_is_set_to_the_measurements(self, Measurement, JsonResponse):
+    def test_response_content_is_set_to_the_measurements(self, Measurement, JsonResponse, build_query):
+        self.request.GET['date'] = '2015-01-01'
 
         measurements = mock.Mock()
         Measurement.to_dict.return_value = measurements
@@ -41,7 +34,7 @@ class TestAPI(TestCase):
 
         JsonResponse.assert_called_once_with(expected_dict)
 
-    def test_returns_json_response(self, Measurement, JsonResponse):
+    def test_returns_json_response(self, Measurement, JsonResponse, build_query):
 
         expected_response = mock.Mock()
         JsonResponse.return_value = expected_response
@@ -49,3 +42,45 @@ class TestAPI(TestCase):
         response = views.api(self.request)
 
         self.assertEqual(response, expected_response)
+
+    def test_creates_query_with_request_args(self, Measurement, JsonResponse, build_query):
+
+        response = views.api(self.request)
+
+        build_query.assert_called_once_with(self.request.GET)
+
+    def test_filters_mesurement_with_query(self, Measurement, JsonResponse, build_query):
+
+        query = {}
+        build_query.return_value = query
+        
+        response = views.api(self.request)
+
+        Measurement.objects.filter.assert_called_once_with(**query)
+        
+class TestBuildQuery(TestCase):
+
+    def test_builds_query_with_date(self):
+
+        request = HttpRequest()
+        request.GET['date'] = '2015-01-01'
+        expected_query = {'datetime__year': 2015, 'datetime__month': 1, 'datetime__day': 1}
+
+        query = views.build_query(request.GET)
+
+        self.assertEqual(query, expected_query)
+
+    def test_builds_query_with_first_and_last_datetime(self):
+
+        request = HttpRequest()
+        request.GET['datetime-first'] = '2015-01-01 00:00:40'
+        request.GET['datetime-last'] = '2015-01-01 00:00:50'
+        expected_query = {
+            'datetime__gte': datetime.datetime(2015, 1, 1, 0, 0, 40),
+            'datetime__lte': datetime.datetime(2015, 1, 1, 0, 0, 50),
+        }
+        
+        query = views.build_query(request.GET)
+
+        self.assertEqual(query, expected_query)
+
--- opentrv_sensor/views.py
+++ opentrv_sensor/views.py
@@ -1,14 +1,29 @@
 import datetime
+from dateutil import parser as date_parser
 from opentrv_sensor.models import Measurement
 from django.http import JsonResponse
 from django.shortcuts import render
+from django.utils.datastructures import MultiValueDictKeyError
 
-# Create your views here.
+def build_query(args):
+    query = {}
+    if 'date' in args:
+        date = datetime.datetime.strptime(args['date'], '%Y-%m-%d')
+        query['datetime__year'] = date.year
+        query['datetime__month'] = date.month
+        query['datetime__day'] = date.day
+    if 'datetime-first' in args and 'datetime-last' in args:
+        datetime_first = date_parser.parse(args['datetime-first'])
+        datetime_last = date_parser.parse(args['datetime-last'])
+        query['datetime__gte'] = datetime_first
+        query['datetime__lte'] = datetime_last
+    return query
 
 def api(request):
     response = {'status': 200, 'content': None, 'errors': []}
-    date = datetime.datetime.strptime(request.GET['date'], '%Y-%m-%d')
-    measurements = Measurement.objects.filter(datetime__year=date.year, datetime__month=date.month, datetime__day=date.day)
+    query = build_query(request.GET)
+    measurements = Measurement.objects.filter(**query)
     measurements = Measurement.to_dict(measurements)
     response['content'] = measurements
+    
     return JsonResponse(response)
