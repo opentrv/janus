520ad7ec863a00ae1b9f5694a244b9e76e178ebe
David Voong <voong.david@gmail.com>
2015-12-02 12:45:55 +0000

    Updated how the opentrv_sensor Measurement creates objects from the UDP message. Can now create from a UDP message and a log message using create_from_log. Log messages include a datetime, UDP messages do not, instead they use the datetime when the message was received.

--- opentrv_sensor/models.py
+++ opentrv_sensor/models.py
@@ -2,6 +2,12 @@ import json
 from dateutil import parser as date_parser
 from django.db import models
 
+def get_current_datetime():
+    pass
+
+def convert_datetime(datetime_string):
+    pass
+
 # Create your models here.
 class Measurement(models.Model):
     '''
@@ -39,23 +45,33 @@ class Measurement(models.Model):
         }
 
     @staticmethod
-    def create_from_udp(msg):
+    def create_from_log(msg):
+
+        datetime, host_id, measurements  = json.loads(msg)
+        msg = msg[msg.index('{'):msg.index('}') + 1]
+        datetime = convert_datetime(datetime)
+        output = Measurement.create_from_udp(msg, datetime=datetime)
+        
+        return output
+    
+    @staticmethod
+    def create_from_udp(msg, datetime=None):
+        output = {'success': [], 'failure': []}
         json_object = json.loads(msg)
-        datetime = date_parser.parse(json_object[0])
-        sensor_id = json_object[2]['@']
         measurements = {}
-        for key, val in json_object[2].iteritems():
-            if key != '@' and key != '+':
+        for key, val in json_object.iteritems():
+            if key == '@':
+                sensor_id = val
+            elif key == '+':
+                continue
+            else:
                 measurements[key] = val
 
-        output = {'success': [], 'failure': []}
         for key, val in measurements.iteritems():
             if '|' in key:
                 type_, units = key.split('|')
             else:
-                type_ = key
-                units = None
-
+                type_, units = (key, None)
             try:
                 type_ = {'vac': 'vacancy',
                          'T': 'temperature',
@@ -67,6 +83,7 @@ class Measurement(models.Model):
                          'vC': 'valve_travel',
                          'O': 'occupancy'
                 }[type_]
+
                 if type_ == 'temperature' or type_ == 'target_temperature':
                     if units == 'C16':
                         val = val / 16.
@@ -84,7 +101,7 @@ class Measurement(models.Model):
                     else:
                         raise Exception('Unrecognised unit of battery')
 
-                measurement = Measurement(datetime=datetime, sensor_id=sensor_id, type=type_, value=val)
+                measurement = Measurement(datetime=get_current_datetime() if datetime == None else datetime, sensor_id=sensor_id, type=type_, value=val)                
                 measurement.save()
                 output['success'].append(measurement)
             except KeyError as e:
@@ -94,4 +111,59 @@ class Measurement(models.Model):
                 })
 
         return output
+
+        # json_object = json.loads(msg)
+        # datetime = date_parser.parse(json_object[0])
+        # sensor_id = json_object[2]['@']
+        # measurements = {}
+        # for key, val in json_object[2].iteritems():
+        #     if key != '@' and key != '+':
+        #         measurements[key] = val
+
+        # output = {'success': [], 'failure': []}
+        # for key, val in measurements.iteritems():
+        #     if '|' in key:
+        #         type_, units = key.split('|')
+        #     else:
+        #         type_ = key
+        #         units = None
+
+        #     try:
+        #         type_ = {'vac': 'vacancy',
+        #                  'T': 'temperature',
+        #                  'L': 'light',
+        #                  'B': 'battery',
+        #                  'v': 'valve_open_percent',
+        #                  'H': 'relative_humidity',
+        #                  'tT': 'target_temperature',
+        #                  'vC': 'valve_travel',
+        #                  'O': 'occupancy'
+        #         }[type_]
+        #         if type_ == 'temperature' or type_ == 'target_temperature':
+        #             if units == 'C16':
+        #                 val = val / 16.
+        #             elif units == 'C':
+        #                 pass
+        #             else:
+        #                 raise Exception('Unrecognised unit of temperature')
+        #         if type_ == 'battery':
+        #             if units == 'cV':
+        #                 val = val * 0.01
+        #             elif units == 'V':
+        #                 pass
+        #             elif units == 'mV':
+        #                 val = val * 0.001
+        #             else:
+        #                 raise Exception('Unrecognised unit of battery')
+
+        #         measurement = Measurement(datetime=datetime, sensor_id=sensor_id, type=type_, value=val)
+        #         measurement.save()
+        #         output['success'].append(measurement)
+        #     except KeyError as e:
+        #         output['failure'].append({
+        #             'type': key,
+        #             'value': val
+        #         })
+
+        # return output
 
--- opentrv_sensor/tests/test_models.py
+++ opentrv_sensor/tests/test_models.py
@@ -44,25 +44,32 @@ class TestInit(TestMeasurement):
     def test(self):
         pass
 
+@mock.patch('opentrv_sensor.models.get_current_datetime')
 @mock.patch('opentrv_sensor.models.Measurement')
 class TestCreateFromUDP(TestMeasurement):
 
-    def test_single_measurement(self, MockMeasurement):
-        msg = '[ "2015-01-01T00:00:43Z", "", {"@":"0a45","+":2,"vac|h":9} ]'
-        expected_datetime = timezone.make_aware(timezone.datetime(2015, 1, 1, 0, 0, 43), dateutil.tz.tzlocal())
-        
-        measurements = Measurement.create_from_udp(msg)
+    def test_supplied_datetime(self, MockMeasurement, get_current_datetime):
+        msg = '{"@":"0a45","+":2,"vac|h":9}'
+        mock_datetime = mock.Mock()
 
-        MockMeasurement.assert_called_once_with(datetime=expected_datetime, sensor_id="0a45", type="vacancy", value=9)
-        self.assertEqual(measurements['success'][0], MockMeasurement.return_value)
+        measurements = Measurement.create_from_udp(msg, mock_datetime)
+
+        MockMeasurement.assert_called_once_with(datetime=mock_datetime, sensor_id="0a45", type="vacancy", value=9)
     
-    def test_multiple_measurements(self, MockMeasurement):
-        msg = '[ "2015-01-01T00:00:43Z", "", {"@":"0a45","+":2,"vac|h":9,"T|C16":201,"L":0} ]'
-        expected_datetime = timezone.make_aware(timezone.datetime(2015, 1, 1, 0, 0, 43), dateutil.tz.tzlocal())
+    def test_single_measurement(self, MockMeasurement, get_current_datetime):
+        msg = '{"@":"0a45","+":2,"vac|h":9}'
+        
+        measurements = Measurement.create_from_udp(msg)
+
+        MockMeasurement.assert_called_once_with(datetime=get_current_datetime(), sensor_id="0a45", type="vacancy", value=9)
+        self.assertEqual(measurements['success'][0], MockMeasurement.return_value)
+
+    def test_multiple_measurements(self, MockMeasurement, get_current_datetime):
+        msg = '{"@":"0a45","+":2,"vac|h":9,"T|C16":201,"L":0}'
         expected_calls = [
-            mock.call(datetime=expected_datetime, sensor_id="0a45", type="vacancy", value=9),
-            mock.call(datetime=expected_datetime, sensor_id="0a45", type="temperature", value=12.5625),
-            mock.call(datetime=expected_datetime, sensor_id="0a45", type="light", value=0)
+            mock.call(datetime=get_current_datetime(), sensor_id="0a45", type="vacancy", value=9),
+            mock.call(datetime=get_current_datetime(), sensor_id="0a45", type="temperature", value=12.5625),
+            mock.call(datetime=get_current_datetime(), sensor_id="0a45", type="light", value=0)
         ]
         mock_measurements = [mock.Mock(), mock.Mock(), mock.Mock()]
         MockMeasurement.side_effect = mock_measurements
@@ -72,13 +79,12 @@ class TestCreateFromUDP(TestMeasurement):
         self.assertEqual(MockMeasurement.call_args_list, expected_calls)
         self.assertEqual(measurements['success'], mock_measurements)
 
-    def test_battery_with_centivolts(self, MockMeasurement):
-        msg = '[ "2015-01-01T00:01:13Z", "", {"@":"819c","T|C16":71,"L":5,"B|cV":256} ]'
-        expected_datetime = timezone.make_aware(timezone.datetime(2015, 1, 1, 0, 1, 13), dateutil.tz.tzlocal())
+    def test_battery_with_centivolts(self, MockMeasurement, get_current_datetime):
+        msg = '{"@":"819c","T|C16":71,"L":5,"B|cV":256}'
         expected_calls = [
-            mock.call(datetime=expected_datetime, sensor_id="819c", type="temperature", value=4.4375),
-            mock.call(datetime=expected_datetime, sensor_id="819c", type="light", value=5),
-            mock.call(datetime=expected_datetime, sensor_id="819c", type="battery", value=2.56)
+            mock.call(datetime=get_current_datetime(), sensor_id="819c", type="temperature", value=4.4375),
+            mock.call(datetime=get_current_datetime(), sensor_id="819c", type="light", value=5),
+            mock.call(datetime=get_current_datetime(), sensor_id="819c", type="battery", value=2.56)
         ]
         mock_measurements = [mock.Mock(), mock.Mock(), mock.Mock()]
         MockMeasurement.side_effect = mock_measurements
@@ -88,14 +94,13 @@ class TestCreateFromUDP(TestMeasurement):
         self.assertEqual(MockMeasurement.call_args_list, expected_calls)
         self.assertEqual(measurements['success'], mock_measurements)
 
-    def test_cumulative_valve_travel(self, MockMeasurement):
-        msg = '[ "2015-01-01T00:01:19Z", "", {"@":"414a","+":4,"vac|h":3,"v|%":0,"tT|C":7,"vC|%":50} ]'
-        expected_datetime = timezone.make_aware(timezone.datetime(2015, 1, 1, 0, 1, 19), dateutil.tz.tzlocal())
+    def test_cumulative_valve_travel(self, MockMeasurement, get_current_datetime):
+        msg = '{"@":"414a","+":4,"vac|h":3,"v|%":0,"tT|C":7,"vC|%":50}'
         expected_calls = [
-            mock.call(datetime=expected_datetime, sensor_id="414a", type="vacancy", value=3),
-            mock.call(datetime=expected_datetime, sensor_id="414a", type="valve_open_percent", value=0),
-            mock.call(datetime=expected_datetime, sensor_id="414a", type="target_temperature", value=7),
-            mock.call(datetime=expected_datetime, sensor_id="414a", type="valve_travel", value=50),
+            mock.call(datetime=get_current_datetime(), sensor_id="414a", type="vacancy", value=3),
+            mock.call(datetime=get_current_datetime(), sensor_id="414a", type="valve_open_percent", value=0),
+            mock.call(datetime=get_current_datetime(), sensor_id="414a", type="target_temperature", value=7),
+            mock.call(datetime=get_current_datetime(), sensor_id="414a", type="valve_travel", value=50),
         ]
         mock_measurements = [mock.Mock(), mock.Mock(), mock.Mock(), mock.Mock()]
         MockMeasurement.side_effect = mock_measurements
@@ -111,12 +116,11 @@ class TestCreateFromUDP(TestMeasurement):
         self.assertEqual(measurements['success'], expected_measurements['success'])
         for failure in expected_measurements['failure']:
             self.assertIn(failure, measurements['failure'])
-        
-    def test_battery_millivolts(self, MockMeasurement):
-        msg = '[ "2015-01-01T00:01:19Z", "", {"@":"414a","+":4,"B|mV":3}]'
-        expected_datetime = timezone.make_aware(timezone.datetime(2015, 1, 1, 0, 1, 19), dateutil.tz.tzlocal())
+
+    def test_battery_millivolts(self, MockMeasurement, get_current_datetime):
+        msg = '{"@":"414a","+":4,"B|mV":3}'
         expected_calls = [
-            mock.call(datetime=expected_datetime, sensor_id="414a", type="battery", value=0.003),
+            mock.call(datetime=get_current_datetime(), sensor_id="414a", type="battery", value=0.003),
         ]
         mock_measurements = [mock.Mock()]
         MockMeasurement.side_effect = mock_measurements
@@ -130,3 +134,23 @@ class TestCreateFromUDP(TestMeasurement):
         self.assertEqual(MockMeasurement.call_args_list, expected_calls)
         self.assertEqual(measurements['success'], expected_measurements['success'])
         self.assertEqual(measurements['failure'], expected_measurements['failure'])
+
+@mock.patch('opentrv_sensor.models.convert_datetime')
+@mock.patch('opentrv_sensor.models.Measurement.create_from_udp')
+class TestCreateFromLog(TestMeasurement):
+    # msg = '[ "2015-01-01T00:01:19Z", "", {"@":"414a","+":4,"B|mV":3}]'
+
+    def test_datetime_string_converted(self, create_from_udp, convert_datetime):
+        msg = '[ "2015-01-01T00:01:19Z", "", {"@":"414a","+":4,"vac|h":3,"v|%":0,"tT|C":7,"vC|%":50} ]'
+
+        measurements = Measurement.create_from_log(msg)
+
+        convert_datetime.assert_called_once_with("2015-01-01T00:01:19Z")
+
+    def test(self, create_from_udp, convert_datetime):
+        msg = '[ "2015-01-01T00:01:19Z", "", {"@":"414a","+":4,"vac|h":3,"v|%":0,"tT|C":7,"vC|%":50} ]'
+
+        measurements = Measurement.create_from_log(msg)
+
+        create_from_udp.assert_called_once_with('{"@":"414a","+":4,"vac|h":3,"v|%":0,"tT|C":7,"vC|%":50}', datetime=convert_datetime())
+        
