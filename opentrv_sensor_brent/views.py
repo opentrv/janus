60b7c333b35b316e82cef27a8551ce31d2912dfa
David Voong <voong.david@gmail.com>
2015-12-09 12:57:55 +0000

    Change brent datetime filters to default to today

--- opentrv_sensor_brent/views.py
+++ opentrv_sensor_brent/views.py
@@ -1,6 +1,13 @@
+import datetime
 from django.shortcuts import render
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'brent/home.html')
+    today = datetime.date.today()
+    today = datetime.datetime(today.year, today.month, today.day)
+    tomorrow = today + datetime.timedelta(days=1)
+    return render(request, 'brent/home.html', {
+        'today': today.isoformat().replace('T', ' '),
+        'tomorrow': tomorrow.isoformat().replace('T', ' ')
+    })
