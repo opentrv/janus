28a8d0e16184ae8ac180a4c1c0d11a407337bc0c
ravindra-appaswamy <ravindra.appaswamy@gmail.com>
2016-02-19 16:47:41 +0000

    WIP updated travis script file

--- travis-script.sh
+++ travis-script.sh
@@ -10,9 +10,9 @@ sudo mkdir $TRAVIS_BUILD_DIR/database/
 sudo mkdir $TRAVIS_BUILD_DIR/logs/
 sudo -u postgres psql -c 'create user opentrv --createdb --superuser' 
 sudo -u postgres psql -c 'create database opentrv_db owner opentrv'
-sudo cp $TRAVIS_BUILD_DIR/templates/gunicorn.conf.j2 /etc/init/opentrv_gunicorn.conf
-sudo cp $TRAVIS_BUILD_DIR/templates/nginx.conf.j2 /etc/nginx/sites-available/opentrv
-sudo ln /etc/nginx/sites-enabled/opentrv /etc/nginx/sites-available/opentrv
-sudo service nginx restart
-sudo service gunicorn restart
-sudo service udp_server restart
+# sudo cp $TRAVIS_BUILD_DIR/templates/gunicorn.conf.j2 /etc/init/opentrv_gunicorn.conf
+# sudo cp $TRAVIS_BUILD_DIR/templates/nginx.conf.j2 /etc/nginx/sites-available/opentrv
+# sudo ln /etc/nginx/sites-enabled/opentrv /etc/nginx/sites-available/opentrv
+# sudo service nginx restart
+# sudo service gunicorn restart
+# sudo service udp_server restart
