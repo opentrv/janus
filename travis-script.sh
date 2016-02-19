937de604b4a9012b0a578964c25a38c91be7daf3
ravindra-appaswamy <ravindra.appaswamy@gmail.com>
2016-02-19 11:26:20 +0000

    WIP: added travis.yml file for CI support

--- travis-script.sh
+++ travis-script.sh
@@ -1,14 +1,16 @@
+echo $TRAVIS_BUILD_DIR
+cd $TRAVIS_BUILD_DIR
 sudo adduser opentrv --system --disabled-password --uid 510
 sudo addgroup --gid 510 opentrv
 sudo adduser --uid 510 opentrv --ingroup opentrv sudo
-sudo mkdir /srv/opentrv
-sudo chown -R opentrv:opentrv /srv/opentrv
-sudo mkdir /srv/opentrv/database/
-sudo mkdir /srv/opentrv/logs/
+# sudo mkdir /srv/opentrv
+sudo chown -R opentrv:opentrv $TRAVIS_BUILD_DIR
+sudo mkdir $TRAVIS_BUILD_DIR/database/
+sudo mkdir $TRAVIS_BUILD_DIR/logs/
 sudo -u postgres psql -c 'create user opentrv --createdb --superuser' 
 sudo -u postgres psql -c 'create database opentrv_db owner opentrv'
-sudo cp srv/opentrv/templates/gunicorn.conf.j2 /etc/init/opentrv_gunicorn.conf
-sudo cp srv/opentrv/templates/nginx.conf.j2 /etc/nginx/sites-available/opentrv
+sudo cp $TRAVIS_BUILD_DIR/templates/gunicorn.conf.j2 /etc/init/opentrv_gunicorn.conf
+sudo cp $TRAVIS_BUILD_DIR/templates/nginx.conf.j2 /etc/nginx/sites-available/opentrv
 sudo ln /etc/nginx/sites-enabled/opentrv /etc/nginx/sites-available/opentrv
 sudo service nginx restart
 sudo service gunicorn restart
