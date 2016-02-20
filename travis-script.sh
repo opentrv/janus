8e61ec6653e70969dbe9e4689c83cabb49aade30
ravindra-appaswamy <ravindra.appaswamy@gmail.com>
2016-02-20 22:43:15 +0000

    WIP travis test scripts

--- travis-script.sh
+++ travis-script.sh
@@ -9,13 +9,13 @@ sudo adduser opentrv --uid 510 --gid 510 --system --no-create-home --disabled-pa
 sudo usermod -aG sudo opentrv
 
 # sudo mkdir /srv/opentrv
-sudo chown -R opentrv:opentrv $TRAVIS_BUILD_DIR
+# sudo chown -R opentrv:opentrv $TRAVIS_BUILD_DIR
 sudo mkdir $TRAVIS_BUILD_DIR/database/
 sudo mkdir $TRAVIS_BUILD_DIR/logs/
 sudo -u postgres psql -c "create user opentrv --createdb --superuser --password 'secret'"
 # sudo -u postgres psql -c "ALTER USER opentrv WITH PASSWORD 'secret';"
 sudo -u postgres psql -c 'create database opentrv_db owner opentrv'
-sudo -u opentrv -s
+# sudo -u opentrv -s
 # sudo cp $TRAVIS_BUILD_DIR/templates/gunicorn.conf.j2 /etc/init/opentrv_gunicorn.conf
 # sudo cp $TRAVIS_BUILD_DIR/templates/nginx.conf.j2 /etc/nginx/sites-available/opentrv
 # sudo ln /etc/nginx/sites-enabled/opentrv /etc/nginx/sites-available/opentrv
