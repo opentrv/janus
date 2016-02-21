c0cdf8a58a0c817ef1b95d925e749350e9f0c047
ravindra-appaswamy <ravindra.appaswamy@gmail.com>
2016-02-21 12:03:29 +0000

    WIP travis test scripts

--- run-tests.sh
+++ run-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 mkdir -p  ../database
-python manage.py migrate
+python manage.py migrate 
 python manage.py runserver &
-python manage.py start_udp_server
+python manage.py start_udp_server &
 python manage.py test
--- travis-script.sh
+++ travis-script.sh
@@ -9,7 +9,7 @@ sudo adduser opentrv --uid 510 --gid 510 --system --no-create-home --disabled-pa
 sudo usermod -aG sudo opentrv
 
 # sudo mkdir /srv/opentrv
-# sudo chown -R opentrv:opentrv $TRAVIS_BUILD_DIR
+sudo chmod -R 775 $TRAVIS_BUILD_DIR
 sudo mkdir $TRAVIS_BUILD_DIR/database/
 sudo mkdir $TRAVIS_BUILD_DIR/logs/
 sudo -u postgres psql -c "create user opentrv --createdb --superuser --password 'secret'"
