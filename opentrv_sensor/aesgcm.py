45cb7ee1492f612411a7e48bab6e9bff77d90041
m-h-w <matthewhwaite@gmail.com>
2016-03-09 12:56:39 +0000

    1. Connecting aesgcm to the udpserver 2. writing a udp packet generator that reads the exiting log files    extracts the original packet and sends to 127.0.0.1:9999

--- opentrv_sensor/aesgcm.py
+++ opentrv_sensor/aesgcm.py
@@ -1,12 +1,16 @@
 
 
 import os
+import binascii
 
 # lifted from https://cryptography.io/en/latest/hazmat/primitives/symmetric-encryption/#cryptography.hazmat.primitives.ciphers.modes.GCM
 from cryptography.hazmat.primitives.ciphers import (
     Cipher, algorithms, modes
 )
 from cryptography.hazmat.backends import default_backend
+
+from datamodel.datamodelquery import SensorLocationQuery
+from datamodel.datamodelquery import SensorQuery
 
 
 # AESGCM encoding 101
@@ -45,12 +49,25 @@ class OpenTRVAesgcmPacket(object):
     
     # Class instance expects the encrypted packet, stored as a bytearray and the  6 bytes of 
     # The 6 byte leaf node ID required for the decrypt (the full ID is 8cbytes long) also stored as a byte array.
-    def __init__ (self,encryptedPacket= bytearray(),sixByteID=bytearray()):  
+    def __init__ (self,encryptedPacket= bytearray(),test=None):  
       
-      #ToDo check the last byte of encrptedPacket is 0x80 (indicating AES-GCM encryption) and throw an exception if not
       self.encryptedPacket = encryptedPacket
-      self.sixByteID = sixByteID 
+      self.test = test
+      # Data object containing the pre-shared sensor ID and the pre-shared aesgcm encryption key
+      if self.test ==None:
+         self.preshared = {
+                        "key": bytarray(),
+                        "sixByteID": bytearray()
+                        }
+         self.getPresharedData()
     
+      else: # test without reading from DB
+          
+        self.preshared = {
+                          "key": bytearray([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),       # The current key in the java test code is 128bits of 0
+                          "sixByteID": bytearray ([0xAA,0xAA,0xAA,0xAA,0x55,0x55])   # 
+                          }
+          
     # constants indicating the location of the fixed location bytes in the header
     FL= 0       #Frame Length is the 0th byte
     FT = 1      #Frame Type
@@ -58,16 +75,40 @@ class OpenTRVAesgcmPacket(object):
     ID_LEN = 2  #ID length = lower nibble of byte 2
     ID = 3      # the ID starts at byte 3 and is IDLEN bytes long
     
+    # This function extracts the 6MSBs of the sensorID and the AESGCM encryption key from the database
+    # To achieve the the function accepts the raw over the air (OTA) packet which contains
+    # a variable number of sensor ID bytes (typically 2 or 4). The OTASensorID
+    # is then used as a 'key' into the database to retrieve the  6 MS bytes of 
+    # the full sensor ID plus the aesgcm encryption key assigned to that sensor.
+     
+    def getPresharedData(self):
+        
+        # extract the sensor ID from the raw packet
+        OTASensorIDLen = (self.encryptedPacket[OpenTRVAesgcmPacket.ID_LEN] & 0x0F)   # mask out the top bits of the byte as they contain a sequence number
+        OTAsensorID = self.encryptedPacket[OpenTRVAesgcmPacket.ID:OTSsensorIDLen]    # copy the variable length address bytes out of the raw OTA packet.
+        
+        #database queries     
+        returned_sensor=SensorQuery().get_sensor_from_partial_node_id(OTAsensorID)
+        fullSensorID  = returned_sensor.node_id
+        
+        sensor_location = SensorLocationQuery().get_current_sensor_location(returned_sensor)    
+        key = sensor_location.aes_key
+        
+        # convert ascii strings to hex
+        self.preshared["sixByteID"].extend(binascii.unhexlify(fullSensorID))
+        self.preshared["key"].extend(binascii.unhexlify(key))
+    
+    def getKey (self):
+        return self.preshared["key"]
       
-  # Retrieve IV/nonce from raw message and other information.  
-     # 4 MSBs of ID. contained in the unencrypted message header
-     # 2 LSBs of ID, that are not sent OTA but magically shared
+     # Retrieve IV/nonce from raw message and other information.  
+     # 6 bytes of ID (2 or more of which will come over the air the rest from a DB lookuo)
      # 3 bytes of resart counter - retrieved from the trailer
      # 3 bytes of tx message counter - retrieved from the trailer
      
     def iv (self):
         # *offsets* into encryptedPacket for various protocol elements
-        bodyLen= (OpenTRVAesgcmPacket.ID+self.encryptedPacket[OpenTRVAesgcmPacket.ID_LEN])   
+        bodyLen= (OpenTRVAesgcmPacket.ID+(self.encryptedPacket[OpenTRVAesgcmPacket.ID_LEN] & 0x0F))   
         bodyStart = bodyLen + 1          #body starts 1 byte after the length byte (which is part of the header)
         trailer = bodyStart + self.encryptedPacket[bodyLen]   
         # Bytes 0-3 of trailer are the restart counter
@@ -79,7 +120,7 @@ class OpenTRVAesgcmPacket(object):
        
         nonce = bytearray()        
         #copy 6 MSBs of ID contained in the sixByteID passed in at class instantiation
-        nonce.extend(self.sixByteID)  
+        nonce.extend(self.preshared ["sixByteID"])  
         #copy the 3 bytes of restart counter
         nonce.extend(self.encryptedPacket[restartCounter:msgCounter])
         #copy the 3 bytes of the message counter
@@ -109,7 +150,7 @@ class OpenTRVAesgcmPacket(object):
         tagOffset = len (self.encryptedPacket) - 17
         tag = self.encryptedPacket[tagOffset:(len(self.encryptedPacket)-1)]
         return (tag)
-
+    
 
 
 def decrypt(key, associated_data, iv, ciphertext, tag):
@@ -121,38 +162,44 @@ def decrypt(key, associated_data, iv, ciphertext, tag):
    # bytes. Consequently all the parameter objects had to be converted to strings, hence the str()
    # around each one.
    # The moral of this story is strong typing is good. 
+    try: 
+        decryptor = Cipher(
+            algorithms.AES(str(key)),
+            modes.GCM(str(iv), str(tag)),
+            backend=default_backend()
+        ).decryptor()
     
-    decryptor = Cipher(
-        algorithms.AES(str(key)),
-        modes.GCM(str(iv), str(tag)),
-        backend=default_backend()
-    ).decryptor()
+        # We put associated_data back in or the tag will fail to verify
+        # when we finalize the decryptor.
+        decryptor.authenticate_additional_data(str(associated_data))
+    
+        # Decryption gets us the authenticated plaintext.
+        # If the tag does not match an InvalidTag exception will be raised.
+        ret= decryptor.update(str(ciphertext)) + decryptor.finalize()
 
-    # We put associated_data back in or the tag will fail to verify
-    # when we finalize the decryptor.
-    decryptor.authenticate_additional_data(str(associated_data))
+    except Exception as e:
+        print (' Failed to de-crypt Measurement with exception: {}: {}'.format(e.__class__.__name__, e))
+        ret = None
+        
+    
+    return ret
 
-    # Decryption gets us the authenticated plaintext.
-    # If the tag does not match an InvalidTag exception will be raised.
-    return decryptor.update(str(ciphertext)) + decryptor.finalize()
-
-
-# Returns the decrypted message body: two status bytes and a quasi JSON object
+# Interface to the AESGCM decryption object. 
+#Returns the decrypted message body: two status bytes and a quasi JSON object
 # when passed an encrypted packet, along with the pre-shared key and the pre-shared
-# 6 byte leafnode ID. The pre-shared values are extracted from a database, using 
-# 4 (or possibly 2) leafnode ID bytes sent in the packet as a key into the database.
-def extractMessageFromEncryptedPacket (encryptedPacket, preshared):
+# 6 byte leaf-node ID. The pre-shared values are extracted from a database, using 
+# 4 (or possibly 2) leaf-node ID bytes sent in the packet as a key into the database.
+def extractMessageFromEncryptedPacket (encryptedPacket,test):
     
     # openTRVAesgcmPacket object operates on the received packet data
-    packet = OpenTRVAesgcmPacket(encryptedPacket,preshared["sixByteID"])
+    packet = OpenTRVAesgcmPacket(encryptedPacket,test)
             
     plainText = decrypt(
-                preshared["key"],
+                packet.getKey(),
                 packet.aad(),
                 packet.iv(),
                 packet.ciphertext(),
                 packet.tag())
-    
     
     return (plainText)
     
@@ -168,21 +215,7 @@ if __name__ == "__main__":
             print ("%02x " % data[i]), # comma at EOL stops new line at every iteration.
         print ('')
 
-    def getPresharedData(encryptedPacket):
-    
-    # TODO: there needs to be a database look up somewhere that takes a partial leaf node ID and looks up the 
-    # 6 byte ID along with the preshared key required for decrypting packets 
-    
-    # What is here now is test vector data generated by SecureFrameTest.java
-    
-        preshared = {
-                     "key": bytearray([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]),       # The current key in the java test code is 128bits of 0
-                     "sixByteID": bytearray ([0xAA,0xAA,0xAA,0xAA,0x55,0x55])   # 
-                     }
-        return (preshared)
-
-
-
+   
     
     #Test packet generated from SecureFrameTest.java with an all 0 key
     encryptedPacket = bytearray ([0x3f,0xcf,0x04,0xaa,0xaa,0xaa,0xaa,0x20,\
@@ -194,16 +227,10 @@ if __name__ == "__main__":
                                   0x51,0x06,0xe1,0x40,0xff,0x29,0x84,0xdf,\
                                   0x71,0xc0,0x48,0x10,0xc7,0xfc,0x80])
     
-    # retrieve the pre-shared key and leaf node ID from the database
-    preshared = getPresharedData(encryptedPacket)
+   
+    TEST = True
     
-    print ('key: '),
-    prettyprint (preshared["key"])
-    print('Leaf node ID: '),
-    prettyprint (preshared ["sixByteID"])
-    print('')
-    
-    packet = OpenTRVAesgcmPacket(encryptedPacket,preshared ["sixByteID"])
+    packet = OpenTRVAesgcmPacket(encryptedPacket,TEST)
     print ('Packet: '),
     prettyprint(encryptedPacket)
     print ('aad: '),
@@ -215,9 +242,7 @@ if __name__ == "__main__":
     print ('tag: '),
     prettyprint (packet.tag())
     
-    
-    
-    plainText=extractMessageFromEncryptedPacket (encryptedPacket, preshared)
+    plainText=extractMessageFromEncryptedPacket (encryptedPacket,TEST)
     
     # ToDo:check Plain text is the same as the input
     input = (b'\x7f\x11' + "{\"b\":1")
@@ -225,6 +250,7 @@ if __name__ == "__main__":
     #if plainText == input:
      #   print ('Test Passed!!')
     
+    #prettyprint(plainText)
     print (plainText)
     
     
