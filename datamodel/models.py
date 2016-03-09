562962649ea067c91b362fc186c525240adcf265
Mark Hill <mark@teos.co.uk>
2016-03-09 16:35:23 +0000

    clean up

--- datamodel/models.py
+++ datamodel/models.py
@@ -1,32 +1,37 @@
-import json
 from dateutil import parser as date_parser
+import json
+
 from django.db import models
 from django.utils import timezone
 
 
 class Sensor(models.Model):
-	node_id = models.CharField(max_length = 50)
+	node_id = models.CharField(max_length=50)
+    # this is upper or lowercase, no spaces, no commas etc.
+    # examples: 3fe453
+    # 845a23
+    # 934e11
 	created = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
-	
+
 	def __unicode__(self):
 		return self.node_id
 
 class SensorMetadata(models.Model):
-	sensor_ref = models.ForeignKey(Sensor, verbose_name='sensor', on_delete=models.CASCADE, blank = False, null = False)
-	sensor_type = models.CharField('type', max_length=50, blank = True)
-	value = models.CharField(max_length=50, blank = True)
-	unit = models.CharField(max_length=50, blank = True)
+	sensor_ref = models.ForeignKey(Sensor, verbose_name='sensor', on_delete=models.CASCADE, blank=False, null=False)
+	sensor_type = models.CharField('type', max_length=50, blank=True)
+	value = models.CharField(max_length=50, blank=True)
+	unit = models.CharField(max_length=50, blank=True)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
-	
+
 	def __unicode__(self):
 		return self.sensor_type
 
 
 class Address(models.Model):
-	address = models.CharField(max_length=300, blank = True, null=True)
-	postcode = models.CharField(max_length=20, blank = True, null=True)
+	address = models.CharField(max_length=300, blank=True, null=True)
+	postcode = models.CharField(max_length=20, blank=True, null=True)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
 
@@ -35,16 +40,16 @@ class Address(models.Model):
 		return self.address
 
 class Location(models.Model):
-	parent_ref = models.ForeignKey('self', on_delete=models.CASCADE, blank = True, null=True, related_name="children")
-	#location = models.CharField(max_length = 32, blank = True)
-	description = models.CharField(max_length = 50, blank = True)
-	#latlong = models.CharField(max_length = 50, blank = True)
-	address_ref = models.ForeignKey(Address, verbose_name='address', blank = True, null = True)
-	#address = models.CharField(max_length=300, blank = True)
-	#floor = models.IntegerField(blank = True)
-	#room = models.IntegerField(blank = True)
-	#wall = models.CharField(max_length = 20, blank = True)
-	#timestamp = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
+	parent_ref = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True, related_name="children")
+	# location = models.CharField(max_length = 32, blank = True)
+	description = models.CharField(max_length=50, blank=True)
+	# latlong = models.CharField(max_length = 50, blank = True)
+	address_ref = models.ForeignKey(Address, verbose_name='address', blank=True, null=True)
+	# address = models.CharField(max_length=300, blank = True)
+	# floor = models.IntegerField(blank = True)
+	# room = models.IntegerField(blank = True)
+	# wall = models.CharField(max_length = 20, blank = True)
+	# timestamp = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
 
@@ -54,31 +59,31 @@ class Location(models.Model):
 
 
 class SensorLocation(models.Model):
-	#sensor_id = models.CharField(max_length = 50) 
-	sensor_ref = models.ForeignKey(Sensor, verbose_name='sensor', blank = True, null = True)
-	location_ref = models.ForeignKey(Location, verbose_name='location', blank = True, null=True)
-	aes_key = models.CharField("AES key", max_length = 256, blank = True)
+	# sensor_id = models.CharField(max_length = 50)
+	sensor_ref = models.ForeignKey(Sensor, verbose_name='sensor', blank=True, null=True)
+	location_ref = models.ForeignKey(Location, verbose_name='location', blank=True, null=True)
+	aes_key = models.CharField("AES key", max_length=256, blank=True)
 	created = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
 	updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
 	last_measurement = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
-	finish = models.DateTimeField(auto_now_add=False, auto_now=False, null = True)	
+	finish = models.DateTimeField(auto_now_add=False, auto_now=False, null=True)
 
 
-#	duration = models.CharField(max_length = 50, blank = True)
+# 	duration = models.CharField(max_length = 50, blank = True)
 
-#	@classmethod
-#	def to_dict(cls, sensor_location):
-#		if hasattr(sensor_location, '__iter__'): #isinstance(measurement, list):
-#			sensor_locations = sensor_location
-#			output = []
-#			for sensor_location in sensor_locations:
-#				output += [cls.to_dict(sensor_location)]
-#			return output
-#		return {
-#			'sensor_id': sensor_location.sensor_id,
-#			'sensor_location': sensor_location.sensor_location,
-#			'aes_key': sensor_location.aes_key,
-#		}
+# 	@classmethod
+# 	def to_dict(cls, sensor_location):
+# 		if hasattr(sensor_location, '__iter__'): #isinstance(measurement, list):
+# 			sensor_locations = sensor_location
+# 			output = []
+# 			for sensor_location in sensor_locations:
+# 				output += [cls.to_dict(sensor_location)]
+# 			return output
+# 		return {
+# 			'sensor_id': sensor_location.sensor_id,
+# 			'sensor_location': sensor_location.sensor_location,
+# 			'aes_key': sensor_location.aes_key,
+# 		}
 
 	def __unicode__(self):
 		return self.aes_key
@@ -86,12 +91,12 @@ class SensorLocation(models.Model):
 
 
 class Measurement(models.Model):
-    sensor_location_ref = models.ForeignKey(SensorLocation, verbose_name='sensor-location', blank = True, null = True)
-    measurement_type = models.CharField(max_length=50, blank = True, null = True)
-    value = models.CharField(max_length=50, blank = True, null = True)
-    value_integer = models.IntegerField("integer value", blank = True, null= True)
-    value_float = models.FloatField("floating point value", blank = True, null= True)
-    unit = models.CharField(max_length=50, blank = True, null =True)
+    sensor_location_ref = models.ForeignKey(SensorLocation, verbose_name='sensor-location', blank=True, null=True)
+    measurement_type = models.CharField(max_length=50, blank=True, null=True)
+    value = models.CharField(max_length=50, blank=True, null=True)
+    value_integer = models.IntegerField("integer value", blank=True, null=True)
+    value_float = models.FloatField("floating point value", blank=True, null=True)
+    unit = models.CharField(max_length=50, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True, auto_now=False, null=True)
     updated = models.DateTimeField(auto_now_add=False, auto_now=True, null=True)
 
@@ -102,7 +107,7 @@ class Measurement(models.Model):
 
     @classmethod
     def to_dict(cls, measurement):
-        if hasattr(measurement, '__iter__'): #isinstance(measurement, list):
+        if hasattr(measurement, '__iter__'):  # isinstance(measurement, list):
             measurements = measurement
             output = []
             for measurement in measurements:
@@ -119,19 +124,19 @@ class Measurement(models.Model):
             'updated': measurement.updated.isoformat(),
         }
 
-	
+
     def __unicode__(self):
         return self.measurement_type
 
 
 
 
-#class ParentModel(models.Model):
-#	Description = models.CharField(max_length=100)
-#	parentId = models.ForeignKey('self', blank = True, null=True,related_name="children")
-	
+# class ParentModel(models.Model):
+# 	Description = models.CharField(max_length=100)
+# 	parentId = models.ForeignKey('self', blank = True, null=True,related_name="children")
 
-#	def __unicode__(self):
-#		return self.Description
-		
+
+# 	def __unicode__(self):
+# 		return self.Description
+
 # Create your models here.
